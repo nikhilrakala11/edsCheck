180148e271c5258db55d17fe1ed04f11ce63f569
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 15:02:36 +0530

    added js file

---
+++ blocks/nikhil/nikhil.js
@@ -0,0 +1,20 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+
+  block.innerHTML = '';
+
+  rows.forEach((row) => {
+    const cols = [...row.children];
+
+    const image = cols[0]?.textContent?.trim();
+    const alt = cols[1]?.textContent?.trim();
+
+    if (image) {
+      const img = document.createElement('img');
+      img.src = image;
+      img.alt = alt || '';
+
+      block.appendChild(img);
+    }
+  });
+}
