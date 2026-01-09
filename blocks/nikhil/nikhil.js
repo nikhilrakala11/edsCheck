2030cfebe3aed90e9b442aff6ef770405becc6fa
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 16:28:05 +0530

    jjjjjjjjjjjjjjjjjjjjjjjjjjj

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -1,29 +1,10 @@
 async function fetchArticleText() {
   const url = 'http://localhost:4502/bin/fetchchoices';
 
-  try {
-    console.log('Calling servlet:', url);
+  const response = await fetch(url);
+  return response.json();
+}
 
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    console.log('Response status:', response.status);
-
-    if (!response.ok) return null;
-
-    const json = await response.json();
-    console.log('Servlet response JSON:', json);
-
-    return {
-      title: json.title || 'View Article',
-      desc: json.desc || 'No Article Found.',
-    };
-  } catch (e) {
-    console.error('Failed to fetch article', e);
-    return '';
-  }
+export default function decorate(block) {
+  fetchArticleText().then(console.log);
 }
