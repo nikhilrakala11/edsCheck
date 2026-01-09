c847f71e8aee320993e32bb13407a64030fa1dad
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 16:25:46 +0530

    newwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -1,28 +1,29 @@
-async function fetchArticleText(articleNumber) {
-  const url = `localhost:4502/bin/fetchchoices`;
+async function fetchArticleText() {
+  const url = 'http://localhost:4502/bin/fetchchoices';
 
   try {
-    const username = 'admin';
-    const password = 'admin';
+    console.log('Calling servlet:', url);
 
-    const authHeader = `Basic ${btoa(`${username}:${password}`)}`;
     const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: authHeader,
       },
     });
+
+    console.log('Response status:', response.status);
+
     if (!response.ok) return null;
 
     const json = await response.json();
+    console.log('Servlet response JSON:', json);
 
     return {
       title: json.title || 'View Article',
       desc: json.desc || 'No Article Found.',
     };
   } catch (e) {
-    console.error(`Failed to fetch article ${articleNumber}`, e);
+    console.error('Failed to fetch article', e);
     return '';
   }
 }
