04622f3e43b392da73fc601fbd889b5fac463788
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 15:24:25 +0530

    juuuuujujujuujuju

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -1,8 +1,18 @@
-export default function decorate(block) {
-  console.log('NIKHIL BLOCK:', block);
-  console.log('CHILDREN:', block.children);
+// export default function decorate(block) {
+//   console.log('NIKHIL BLOCK:', block);
+//   console.log('CHILDREN:', block.children);
 
-  [...block.children].forEach((row, i) => {
-    console.log(`ROW ${i}:`, row.innerHTML);
+//   [...block.children].forEach((row, i) => {
+//     console.log(`ROW ${i}:`, row.innerHTML);
+//   });
+// }
+export default function decorate(block) {
+  const rows = [...block.children];
+
+  rows.forEach(row => {
+    const img = row.querySelector('img');
+    const alt = img?.alt;
+    console.log(alt);
   });
 }
+
