1d225efe6493e3cee24d48641e9a14a84f10c531
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 17:27:59 +0530

    ytytyt

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -1,51 +1,11 @@
-async function fetchArticleText() {
-    const url = 'http://localhost:4502/new/a/fetchchoices';
+async function fetchArticleText() 
+{
+  const API_URL = 'http://localhost:4502/new/a/fetchchoices';
+  const response = await fetch(API_URL);
 
-    const response = await fetch(url, {
-        credentials: 'same-origin', // IMPORTANT for AEM
-        headers: {
-            'Accept': 'application/json'
-        }
-    });
-    console.log(response,"ooooooooooooooooooooooooooooooo");
-    console.log(response.status);
-    console.log(response.status);
+  if (!response.ok) {
+    throw new Error(`API failed: ${response.status}`);
+  }
 
-    // if (!response.ok) {
-    //     throw new Error(`API failed: ${response.status}`);
-    // }
-
-    // const jsontext = await response.json(); // ✅ read raw text first
-    const text = await response.text(); // ✅ read raw text first
-    console.log(text);
-
-    // if (!text) {
-    //     throw new Error('Empty response from servlet');
-    // }
-
-    return JSON.parse(text); // ✅ safe parse
-}
-
-export default async function decorate(block) {
-    block.innerHTML = '<p>Loading...</p>';
-
-    try {
-        const data = await fetchArticleText();
-        console.log('Servlet data:', data);
-
-        const wrapper = document.createElement('div');
-        wrapper.className = 'article-wrapper';
-
-        const title = document.createElement('h3');
-        title.textContent = data.title || 'No title';
-
-        const desc = document.createElement('p');
-        desc.textContent = data.desc || 'No description';
-
-        wrapper.append(title, desc);
-        block.replaceChildren(wrapper);
-    } catch (e) {
-        console.error(e);
-        block.innerHTML = '<p>Failed to load article.</p>';
-    }
+  return response.json();
 }
