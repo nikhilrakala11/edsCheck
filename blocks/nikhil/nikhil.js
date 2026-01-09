43ed7547750f4b7e5513eefa68fc6ff47423bdee
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-09 15:12:53 +0530

    nikhil.js added

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -1,20 +1,8 @@
 export default function decorate(block) {
-  const rows = [...block.children];
+  console.log('NIKHIL BLOCK:', block);
+  console.log('CHILDREN:', block.children);
 
-  block.innerHTML = '';
-
-  rows.forEach((row) => {
-    const cols = [...row.children];
-
-    const image = cols[0]?.textContent?.trim();
-    const alt = cols[1]?.textContent?.trim();
-
-    if (image) {
-      const img = document.createElement('img');
-      img.src = image;
-      img.alt = alt || '';
-
-      block.appendChild(img);
-    }
+  [...block.children].forEach((row, i) => {
+    console.log(`ROW ${i}:`, row.innerHTML);
   });
 }
