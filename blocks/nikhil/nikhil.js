4ab13fe7e3d7df33a30a1b5d86f0371eea181f32
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-01-10 14:45:54 +0530

    uyuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- blocks/nikhil/nikhil.js
+++ blocks/nikhil/nikhil.js
@@ -14,33 +14,3 @@ async function fetchArticleText() {
   return response.json();
 }
 export default fetchArticleText;
-
-// async function fetchArticleText(articleNumber) {
-//   const url = `localhost:4502/bin/abc.json`;
-
-//   try {
-//     const username = 'admin';
-//     const password = 'admin';
-
-//     const authHeader = `Basic ${btoa(`${username}:${password}`)}`;
-//     const response = await fetch(url, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: authHeader,
-//       },
-//     });
-//     if (!response.ok) return null;
-
-//     const json = await response.json();
-//     console.log(json);
-
-//     return {
-//       title: json.title || 'View Article',
-//       desc: json.desc || 'No Article Found.',
-//     };
-//   } catch (e) {
-//     console.error(`Failed to fetch article ${articleNumber}`, e);
-//     return '';
-//   }
-// }
